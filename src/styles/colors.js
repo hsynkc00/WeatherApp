ae1ebfcc0633385c5d50de2153f3aa0d589090d9
Hüseyin Koç <58426496+hsynkc00@users.noreply.github.com>
2024-04-21 23:05:25 +0300

    colors structure added

---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+export const colors = {
+  blue_light: "#8FB2F5",
+  night_gray: '#1E1E29',
+  gray_900: "#13131A",
+  gray_800: "#16161F",
+  gray_700: "#1C1C27",
+  gray_600: "#22222F",
+  gray_500: "#3B3B54",
+  gray_400: "#7F7F98",
+  gray_300: "#ABABC4",
+  gray_200: "#BFBFD4",
+  gray_100: "#FAFAFA",
+  white: "#FFFFFF",
+  transparent: "transparent",
+};
